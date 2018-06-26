c681e3d1e41fa69bc17178286fa653a2b360a9d4
lpintado <lpintado@tiempodevelopment.com>
2018-06-26 17:52:18 -0500

    again change ignore

---
+++ Samples.MVVM/Samples.Repository/ICustomer.cs
@@ -0,0 +1,14 @@
+﻿using Sample.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sample.Repository
+{
+    public interface ICustomer
+    {
+        List<Customer> Get();
+        void Add();
+        void Update();
+    }
+}
