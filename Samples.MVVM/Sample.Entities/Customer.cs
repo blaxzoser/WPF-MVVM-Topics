12d7a23c45dd62b7fa6a0fe1a4447f47d51c00c9
lpintado <lpintado@tiempodevelopment.com>
2018-06-26 17:13:11 -0500

    Added new proyect

---
+++ Samples.MVVM/Sample.Entities/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sample.Entities
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public int Phone { get; set; }
+        public string LastName { get; set; }
+
+
+    }
+}
