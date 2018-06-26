c64b15bd6454ee42fbd36309ea7291b9036d6bb5
lpintado <lpintado@tiempodevelopment.com>
2018-06-26 16:58:09 -0500

    added new proyect

---
+++ Samples.MVVM/Sample.Repository/CustomerRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Sample.Entities;
+
+namespace Sample.Repository
+{
+    public class CustomerRepository : ICustomer
+    {
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> Get()
+        {
+            List<Customer> newList = new List<Customer>();
+            newList.Add(new Customer() { LastName = "Pintado Goicochea", Name = "Luis", Phone = 234234 });
+            newList.Add(new Customer() { LastName = "Perez GOdinez", Name = "Maria", Phone = 45678923 });
+            newList.Add(new Customer() { LastName = "Mercado Ortiz", Name = "Julieta", Phone = 456789126547 });
+            return newList;
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Samples.MVVM/Samples.MVVM/Command/RelayCommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Samples.MVVM.Command
+{
+    public class RelayCommand : ICommand
+    {
+        Action _tarjetExecuteMethod;
+        Func<bool> _tarjetCanExecuteMethod;
+
+        public RelayCommand(Action excecutedMethod)
+        {
+            _tarjetExecuteMethod = excecutedMethod;
+        }
+        public RelayCommand(Action excecutedMethod, Func<bool> canExecuteMethod)
+        {
+            _tarjetExecuteMethod = excecutedMethod;
+            _tarjetCanExecuteMethod = canExecuteMethod;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        #region ICommand Members
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        public bool CanExecute(object parameter)
+        {
+            if(_tarjetCanExecuteMethod != null)
+            {
+                return _tarjetCanExecuteMethod();
+            }
+            if(_tarjetExecuteMethod != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            if(_tarjetExecuteMethod != null)
+            { _tarjetExecuteMethod(); }
+        }
+
+       
+
+        #endregion
+
+    }
+}
