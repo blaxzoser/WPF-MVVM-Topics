6ae44fa74de83a17fb53380497460cf60cd44c14
lpintado <lpintado@tiempodevelopment.com>
2019-02-20 17:25:25 -0600

    Added changes

---
+++ Samples.MVVM/Sample.Entities/Child.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Entities
+{
+    public class Child
+    {
+        public string Name { get; set; }
+        public  List<Child> Children { get; set; }
+
+    }
+}
